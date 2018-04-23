2a181e0298cabc87a8a36e5b0b11fd60bd9bd523
Vlad Korzun <vladyslavkorzun@gmail.com>
2018-04-23 17:18:52 +0300

    Initial Commit

---
+++ cocoapods-githooks-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cocoapods-githooks-demo
+//
+//  Created by Vladyslav Korzun on 4/23/18.
+//  Copyright © 2018 Vladyslav Korzun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
